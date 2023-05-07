b4a3969080763a4e1961d497ea3c1fe87e9a5373
Conor Dunne <conordunne@outlook.ie>
2023-05-07 22:04:21 +0000

    Connect to mongoDB

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb+srv://conordunne:npm_theOdinProject@cluster0.dqdaiuh.mongodb.net/local_library?retryWrites=true&w=majority";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+  console.log('MongoDB connection successful!');
+}
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
