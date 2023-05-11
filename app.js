4e3bfc5e0f66ef041bc784762ac11955c9f5733c
Conor Dunne <conordunne@outlook.ie>
2023-05-11 00:59:47 +0000

    Fix typo

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const logger = require('morgan');
 // Load environment variables from .env file
 require('dotenv').config();
 
+// import routes
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog");
 
 const app = express();
 
@@ -37,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
